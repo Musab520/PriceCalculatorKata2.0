f2af6943ef70aee196ad51247fb9bc63a81ac7b4
Musab Abuasi <abuasimusab@gmail.com>
2022-07-31 15:35:17 +0300

    Requiremnt EXPENSES PArt 1

---
+++ Repositories/CostRepository.cs
@@ -0,0 +1,17 @@
+﻿using PriceCalculatorKata2._0.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PriceCalculatorKata2._0.Repositories
+{
+    public class CostRepository
+    {
+        public List<Cost> costs = new List<Cost>
+        {
+            new Cost("Packaging Cost",0.01,true),new Cost("Transport Cost",2.2,false)
+        };
+    }
+}
